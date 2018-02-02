4ea13bc0bf29d4acb1e6ebcc5aafabaa0a66849b
Unknown <maxime.dedobbeleer@gmail.com>
2018-02-02 12:31:10 +0100

    fixed that shit if broken blame sam

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,39 +6,70 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Events</title>
+
   <link rel="stylesheet" type="text/css" href="../css/bulma.css">
+  <link rel="stylesheet" type="text/css" href="../css/bulma2.css">
   <link rel="stylesheet" type="text/css" href="../css/aside.css">
   <link rel="stylesheet" type="text/css" href="../css/grid-gallery.css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
-  <script async type="text/javascript" src="../js/bulma.js"></script>
 </head>
 <body>
   <nav class="nav is-dark has-shadow is-hidden-tablet" id="top">
     <div class="container">
-      <span class="nav-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
+      <div class="subcontainer">
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div class="secondsubcontainer">
+        <ul class="ulsearch nobullets isflex justify_stuff">
+          <div class="search">
+      <span class="fa fa-search"></span>
+      <input placeholder="Search term">
+    </div>
+        </ul>
+      </div>
+
       <div class="nav-right nav-menu is-hidden-tablet">
-        <a class="nav-item is-tab is-active">
-          Events
-        </a>
-        <a class="nav-item is-tab">
-          My meetings
-        </a>
-        <a class="nav-item is-tab">
-          My events
-        </a>
-        <a class="nav-item is-tab">
-          Newsfeed
-        </a>
+        <div class="fixit">
+        <div class="sectionfix">
+        <a href="#" class="item active"><span class="icon"><i class="fa fa-calendar-alt"></i></span><span class="name">Events</span></a>
+        </div>
+        <div class="sectionfix">
+        <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">My meetings</span></a>
+        </div>
+        <div class="sectionfix">
+        <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">My events</span></a>
+        </div>
+        </div>
+        <div class="fixit">
+          <div class="sectionfix">
+        <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Newsfeed</span></a>
+          </div>
+          <div class="sectionfix">
+        <a href="#" class="item active"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Account management</span></a>
+          </div>
+          <div class="sectionfix">
+        <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">Meeting management</span></a>
+          </div>
+        </div>
+        <div class="fixit">
+          <div class="sectionfix">
+        <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">Event management</span></a>
+          </div>
+          <div class="sectionfix">
+        <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Admin newsfeed</span></a>
+          </div>
+        </div>
+
       </div>
     </div>
   </nav>
   <div class="columns">
-    <aside class="column is-3 aside hero is-fullheight is-hidden-mobile">
-      <div>
+    <aside class="column is-2 aside hero is-fullheight is-hidden-mobile">
+      <div class="fixleft">
         <div class="account has-text-centered">
           <a href="#">
             <figure class="avatar">
@@ -45,71 +77,41 @@
             </figure>
           </a>
         </div>
-        <div class="main scroll">
+        <div class="main">
           <div class="title">Main</div>
           <a href="#" class="item active"><span class="icon"><i class="fa fa-calendar-alt"></i></span><span class="name">Events</span></a>
           <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">My meetings</span></a>
           <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">My events</span></a>
-          <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Newsfeed</span></a>
-<br>
-<br>
-          <div class="title">Event management</div>
-          <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-alt"></i></span><span class="name">Make an event</span></a>
-          <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">My events</span></a>
-          <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">Finished events</span></a>
-<br>
-<br>
-          <a href="admin.php" class="title"><i class="fa fa-cog"></i>  Admin </a>
-          <a href="admin.php#account" class="item"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Account management</span></a>
-          <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">Meeting management</span></a>
           <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">Event management</span></a>
+        </div>
+        <div class="main">
+          <div class="title"><i class="fa fa-cog"></i>  Admin</div>
+          <a href="#" class="item active"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Account management</span></a>
+          <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">Meeting management</span></a>
+          <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Newsfeed</span></a>
           <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Admin newsfeed</span></a>
         </div>
       </div>
     </aside>
-    <div class="content column is-9">
-      <div class="nav menu search">
-        <div class="container">
-          <!-- Main container -->
-          <nav class="level is-fullwidth is-hidden-mobile">
-            <!-- Left side -->
-            <div class="level-left">
-              <div class="level-item is-fullwidth">
-                <div class="field has-addons is-fullwidth">
-                  <p class="control">
-                    <input class="input is-fullwidth" type="text" placeholder="Find a post">
-                  </p>
+    <div class="content column is-10">
+      <ul class="ulsearch nobullets isflex justify_stuff is-hidden-mobile">
+        <div class="search">
+    <span class="fa fa-search"></span>
+    <input placeholder="Search term">
+  </div>
+      </ul>
 
-                </div>
-              </div>
-            </div>
-
-            <!-- Right side -->
-            <div class="level-right">
-              <i class="fa fa-filter filter"></i>
-              <p class="control">
-                <button class="button">
-                  Search
-                </button>
-              </p>
-            </div>
-          </nav>
-        </div>
-      </div>
-      <div class="filter-item">
-        <h1>Filter Items</h1>
-      </div>
       <div class="section scroll">
         <div class="columns"> <!--  Max 2 items -->
           <div class="column is-6">
             <div class="panel">
-              <p class="is-marginless ">
+              <p class="is-marginless">
                 <img src="https://placehold.it/600x300">
               </p>
               <div class="panel-block">
                 <div class="columns">
                   <div class="column">
-                    <div class="panel-block-item">Eventname</div>
+                    <div class="panel-block-item">Eventname als het lang is fucked het</div>
                   </div>
                   <div class="column has-text-right">
                     <div class="panel-block-item">2 <i class="fa fa-user"></i></div>
@@ -214,8 +216,39 @@
             </div>
           </div>
         </div>
+
+        <nav class="pagination nobullets" role="navigation" aria-label="pagination">
+          <a class="pagination-previous nobullets">Previous</a>
+          <a class="pagination-next nobullets">Next page</a>
+          <ul class="pagination-list nobullets">
+            <li>
+              <a class="pagination-link" aria-label="Goto page 1">1</a>
+            </li>
+            <li>
+              <span class="pagination-ellipsis">&hellip;</span>
+            </li>
+            <li>
+              <a class="pagination-link" aria-label="Goto page 45">45</a>
+            </li>
+            <li>
+              <a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
+            </li>
+            <li>
+              <a class="pagination-link" aria-label="Goto page 47">47</a>
+            </li>
+            <li>
+              <span class="pagination-ellipsis">&hellip;</span>
+            </li>
+            <li>
+              <a class="pagination-link" aria-label="Goto page 86">86</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </div>
+
+  <script async type="text/javascript" src="../js/bulma.js"></script>
+
 </body>
 </html>
