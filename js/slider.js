e735dcfea783564194a42a9a7535fa1755db3e07
Unknown <maxime.dedobbeleer@gmail.com>
2018-02-02 12:40:24 +0100

    get rikt rick

--- js/slider.js
+++ js/slider.js
@@ -1,77 +1,59 @@
-jssor_1_slider_init = function() {
+$("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 1440,
+    step: 5,
+    values: [540, 1020],
+    slide: function (e, ui) {
+        var hours1 = Math.floor(ui.values[0] / 60);
+        var minutes1 = ui.values[0] - (hours1 * 60);
 
-
-    var jssor_1_SlideoTransitions = [
-      [{b:0,d:600,y:-290,e:{y:27}}],
-      [{b:0,d:1000,y:185},{b:1000,d:500,o:-1},{b:1500,d:500,o:1},{b:2000,d:1500,r:360},{b:3500,d:1000,rX:30},{b:4500,d:500,rX:-30},{b:5000,d:1000,rY:30},{b:6000,d:500,rY:-30},{b:6500,d:500,sX:1},{b:7000,d:500,sX:-1},{b:7500,d:500,sY:1},{b:8000,d:500,sY:-1},{b:8500,d:500,kX:30},{b:9000,d:500,kX:-30},{b:9500,d:500,kY:30},{b:10000,d:500,kY:-30},{b:10500,d:500,c:{x:125.00,t:-125.00}},{b:11000,d:500,c:{x:-125.00,t:125.00}}],
-      [{b:0,d:600,x:535,e:{x:27}}],
-      [{b:-1,d:1,o:-1},{b:0,d:600,o:1,e:{o:5}}],
-      [{b:-1,d:1,c:{x:250.0,t:-250.0}},{b:0,d:800,c:{x:-250.0,t:250.0},e:{c:{x:7,t:7}}}],
-      [{b:-1,d:1,o:-1},{b:0,d:600,x:-570,o:1,e:{x:6}}],
-      [{b:-1,d:1,o:-1,r:-180},{b:0,d:800,o:1,r:180,e:{r:7}}],
-      [{b:0,d:1000,y:80,e:{y:24}},{b:1000,d:1100,x:570,y:170,o:-1,r:30,sX:9,sY:9,e:{x:2,y:6,r:1,sX:5,sY:5}}],
-      [{b:2000,d:600,rY:30}],
-      [{b:0,d:500,x:-105},{b:500,d:500,x:230},{b:1000,d:500,y:-120},{b:1500,d:500,x:-70,y:120},{b:2600,d:500,y:-80},{b:3100,d:900,y:160,e:{y:24}}],
-      [{b:0,d:1000,o:-0.4,rX:2,rY:1},{b:1000,d:1000,rY:1},{b:2000,d:1000,rX:-1},{b:3000,d:1000,rY:-1},{b:4000,d:1000,o:0.4,rX:-1,rY:-1}]
-    ];
-
-    var jssor_1_options = {
-      $AutoPlay: 1,
-      $Idle: 3000,
-      $Cols: 1,
-      $Align: 0,
-      $CaptionSliderOptions: {
-        $Class: $JssorCaptionSlideo$,
-        $Transitions: jssor_1_SlideoTransitions,
-        $Breaks: [
-          [{d:2000,b:1000}]
-        ]
-      },
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    };
-
-    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+        if (hours1.length == 1) hours1 = '0' + hours1;
+        if (minutes1.length == 1) minutes1 = '0' + minutes1;
+        if (minutes1 == 0) minutes1 = '00';
+        if (hours1 >= 12) {
+            if (hours1 == 12) {
+                hours1 = hours1;
+                minutes1 = minutes1 + " PM";
+            } else {
+                hours1 = hours1 - 12;
+                minutes1 = minutes1 + " PM";
+            }
+        } else {
+            hours1 = hours1;
+            minutes1 = minutes1 + " AM";
+        }
+        if (hours1 == 0) {
+            hours1 = 12;
+            minutes1 = minutes1;
+        }
 
 
 
-    function ScaleSlider() {
-      w = window.innerWidth;
-      h = window.innerHeight;
-      var size = document.getElementById("jssor_1");
-      var picsize = document.getElementById("slider");
+        $('.slider-time').html(hours1 + ':' + minutes1);
 
-      if (w>1260) {
-        size.style.width="800px";
-        size.style.height="400px";
-        picsize.style.width="800px";
-        picsize.style.height="400px";
-      }
-      else if (w<1260 && w>=900) {
-        size.style.width="500px";
-        size.style.height="200px";
-        picsize.style.width="500px";
-        picsize.style.height="200px";
-      }
-      else if (w<900 && w>=770) {
-        alert("This is between 900 and 770.");
-        size.style.width="350px";
-        size.style.height="150px";
-        picsize.style.width="350px";
-        picsize.style.height="150px";
-      }
-      else if (w<770) {
-        alert("This is smaller than 770.");
-      }
+        var hours2 = Math.floor(ui.values[1] / 60);
+        var minutes2 = ui.values[1] - (hours2 * 60);
+
+        if (hours2.length == 1) hours2 = '0' + hours2;
+        if (minutes2.length == 1) minutes2 = '0' + minutes2;
+        if (minutes2 == 0) minutes2 = '00';
+        if (hours2 >= 12) {
+            if (hours2 == 12) {
+                hours2 = hours2;
+                minutes2 = minutes2 + " PM";
+            } else if (hours2 == 24) {
+                hours2 = 11;
+                minutes2 = "59 PM";
+            } else {
+                hours2 = hours2 - 12;
+                minutes2 = minutes2 + " PM";
+            }
+        } else {
+            hours2 = hours2;
+            minutes2 = minutes2 + " AM";
+        }
+
+        $('.slider-time2').html(hours2 + ':' + minutes2);
     }
-
-
-    $Jssor$.$AddEvent(window, "load", ScaleSlider);
-    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-    /*#endregion responsive code end*/
-};
+});
