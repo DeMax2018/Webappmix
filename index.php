82785618309fb00fe0e4a37c3dbd4dc722d84879
Unknown <maxime.dedobbeleer@gmail.com>
2018-02-26 23:33:10 +0100

    Index.html finished

--- index.php
+++ index.php
@@ -172,35 +172,36 @@ include"conn.php";
 
 
           </div>
+          <nav class="pagination nobullets" role="navigation" aria-label="pagination">
+            <a class="pagination-previous nobullets">Previous</a>
+            <a class="pagination-next nobullets">Next page</a>
+            <ul class="pagination-list nobullets">
+              <li>
+                <a class="pagination-link" aria-label="Goto page 1">1</a>
+              </li>
+              <li>
+                <span class="pagination-ellipsis">&hellip;</span>
+              </li>
+              <li>
+                <a class="pagination-link" aria-label="Goto page 45">45</a>
+              </li>
+              <li>
+                <a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
+              </li>
+              <li>
+                <a class="pagination-link" aria-label="Goto page 47">47</a>
+              </li>
+              <li>
+                <span class="pagination-ellipsis">&hellip;</span>
+              </li>
+              <li>
+                <a class="pagination-link" aria-label="Goto page 86">86</a>
+              </li>
+            </ul>
+          </nav>
         </div>
 
-        <nav class="pagination nobullets" role="navigation" aria-label="pagination">
-          <a class="pagination-previous nobullets">Previous</a>
-          <a class="pagination-next nobullets">Next page</a>
-          <ul class="pagination-list nobullets">
-            <li>
-              <a class="pagination-link" aria-label="Goto page 1">1</a>
-            </li>
-            <li>
-              <span class="pagination-ellipsis">&hellip;</span>
-            </li>
-            <li>
-              <a class="pagination-link" aria-label="Goto page 45">45</a>
-            </li>
-            <li>
-              <a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
-            </li>
-            <li>
-              <a class="pagination-link" aria-label="Goto page 47">47</a>
-            </li>
-            <li>
-              <span class="pagination-ellipsis">&hellip;</span>
-            </li>
-            <li>
-              <a class="pagination-link" aria-label="Goto page 86">86</a>
-            </li>
-          </ul>
-        </nav>
+
       </div>
     </div>
   </div>
