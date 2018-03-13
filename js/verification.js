1f0f05eafa673cdc54d77fe9f1989eb945124700
Unknown <maxime.dedobbeleer@gmail.com>
2018-03-13 12:19:35 +0000

    Verification

---
+++ js/verification.js
@@ -0,0 +1,7 @@
+function joepie() {
+  var button = document.querySelector('.jej');
+  var green = document.querySelector('.yes');
+  var red = document.querySelector('.no');
+    red.classList.toggle('is-not-displayed');
+    green.classList.toggle('is-displayed');
+}
