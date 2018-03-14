51291c513b54848df0135622dca0e90cb552bea4
nicklangens <nl210999@stud.miniemeninstituut.be>
2018-03-14 11:40:19 +0100

    fix sliderbar for the fifth time
    pls no more

--- bookaroom.php
+++ bookaroom.php
@@ -479,101 +479,96 @@ loadfunctionsshow();
     }
 
     </script>
-<script src="js/sliderdate.js" type="text/javascript"></script>
-        <script src='//static.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script><script src='https://unpkg.com/vue/dist/vue.js'></script><script src='https://unpkg.com/vuetify/dist/vuetify.min.js'></script>
-        <script >
+    <script src="js/sliderdate.js" type="text/javascript"></script>
+            <script src='//static.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script><script src='https://unpkg.com/vue/dist/vue.js'></script><script src='https://unpkg.com/vuetify/dist/vuetify.min.js'></script>
+            <script >new Vue({
+              el: '#app',
+              data: () => ({
+                  step:1,
+                  registration:{
+                    name:null,
+                    email:null,
+                    street:null,
+                    city:null,
+                    state:null,
+                    numtickets:0,
+                    shirtsize:'XL'
+                  },
+                  sizes:['S','M','L','XL']
+              }),
+              methods:{
+                submit() {
+                  alert('This is the post. Blah');
+                }
+              }
+            })
+            $("#slider-range").slider({
+                range: true,
+                min: 0,
+                max: 1440,
+                step: 5,
+                values: [540, 1020],
+                slide: function (e, ui) {
+                    var hours1 = Math.floor(ui.values[0] / 60);
+                    var minutes1 = ui.values[0] - (hours1 * 60);
 
-        $("#slider-range").slider({
-            range: true,
-            min: 0,
-            max: 1440,
-            step: 5,
-            values: [540, 1020],
-            slide: function (e, ui) {
-                var hours1 = Math.floor(ui.values[0] / 60);
-                var minutes1 = ui.values[0] - (hours1 * 60);
-
-                if (hours1.length == 1) hours1 = '0' + hours1;
-                if (minutes1.length == 1) minutes1 = '0' + minutes1;
-                if (minutes1 == 0) minutes1 = '00';
-                if (hours1 >= 12) {
-                    if (hours1 == 12) {
+                    if (hours1.length == 1) hours1 = '0' + hours1;
+                    if (minutes1.length == 1) minutes1 = '0' + minutes1;
+                    if (minutes1 == 0) minutes1 = '00';
+                    if (hours1 >= 12) {
+                        if (hours1 == 12) {
+                            hours1 = hours1;
+                            minutes1 = minutes1 + " PM";
+                        } else {
+                            hours1 = hours1 - 12;
+                            minutes1 = minutes1 + " PM";
+                        }
+                    } else {
                         hours1 = hours1;
-                        minutes1 = minutes1 + " PM";
-                    } else {
-                        hours1 = hours1 - 12;
-                        minutes1 = minutes1 + " PM";
+                        minutes1 = minutes1 + " AM";
                     }
-                } else {
-                    hours1 = hours1;
-                    minutes1 = minutes1 + " AM";
-                }
-                if (hours1 == 0) {
-                    hours1 = 12;
-                    minutes1 = minutes1;
-                }
+                    if (hours1 == 0) {
+                        hours1 = 12;
+                        minutes1 = minutes1;
+                    }
 
 
 
-                $('.slider-time').html(hours1 + ':' + minutes1);
+                    $('.slider-time').html(hours1 + ':' + minutes1);
 
-                var hours2 = Math.floor(ui.values[1] / 60);
-                var minutes2 = ui.values[1] - (hours2 * 60);
+                    var hours2 = Math.floor(ui.values[1] / 60);
+                    var minutes2 = ui.values[1] - (hours2 * 60);
 
-                if (hours2.length == 1) hours2 = '0' + hours2;
-                if (minutes2.length == 1) minutes2 = '0' + minutes2;
-                if (minutes2 == 0) minutes2 = '00';
-                if (hours2 >= 12) {
-                    if (hours2 == 12) {
+                    if (hours2.length == 1) hours2 = '0' + hours2;
+                    if (minutes2.length == 1) minutes2 = '0' + minutes2;
+                    if (minutes2 == 0) minutes2 = '00';
+                    if (hours2 >= 12) {
+                        if (hours2 == 12) {
+                            hours2 = hours2;
+                            minutes2 = minutes2 + " PM";
+                        } else if (hours2 == 24) {
+                            hours2 = 11;
+                            minutes2 = "59 PM";
+                        } else {
+                            hours2 = hours2 - 12;
+                            minutes2 = minutes2 + " PM";
+                        }
+                    } else {
                         hours2 = hours2;
-                        minutes2 = minutes2 + " PM";
-                    } else if (hours2 == 24) {
-                        hours2 = 11;
-                        minutes2 = "59 PM";
-                    } else {
-                        hours2 = hours2 - 12;
-                        minutes2 = minutes2 + " PM";
+                        minutes2 = minutes2 + " AM";
                     }
-                } else {
-                    hours2 = hours2;
-                    minutes2 = minutes2 + " AM";
+
+                    $('.slider-time2').html(hours2 + ':' + minutes2);
                 }
-
-                $('.slider-time2').html(hours2 + ':' + minutes2);
-            }
-        });
+            });
 
 
-        </script>
-    </div>
+            </script>
+        </div>
 
-  </div>
-<div id="jsVue">
-  <script type="text/javascript">
-  new Vue({
-    el: '#app',
-    data: () => ({
-        step:1,
-        registration:{
-          name:null,
-          email:null,
-          street:null,
-          city:null,
-          state:null,
-          numtickets:0,
-          shirtsize:'XL'
-        },
-        sizes:['S','M','L','XL']
-    }),
-    methods:{
-      submit() {
-        alert('This is the post. Blah');
-      }
-    }
-  })
-  </script>
-</div>
-  <script async type="text/javascript" src="../js/bulma.js"></script>
+      </div>
 
-</body>
-</html>
+      <script async type="text/javascript" src="../js/bulma.js"></script>
+
+    </body>
+    </html>
