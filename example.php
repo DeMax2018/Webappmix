b802c15851a6a55645d0942e106c304506abe13b
nicklangens <nl210999@stud.miniemeninstituut.be>
2018-03-21 13:56:03 +0100

    merge dees

--- example.php
+++ example.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <script type="text/javascript" src="javatest.js"></script>
-    <script type="text/javascript" src="js/ajaxjquery.js"></script>
-    <title></title>
-  </head>
-  <body onload="test();"
-
-  </body>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<script src='https://cdn.jsdelivr.net/jquery.cloudinary/1.0.18/jquery.cloudinary.js' type='text/javascript'></script>
+<script src="//widget.cloudinary.com/global/all.js" type="text/javascript"></script>
+<script src="/script.js"></script>
+<title>AJAX File Upload With Cloudinary</title>
+</head>
+<body>
+<form id="file-form" action="fileUpload.php" method="post" enctype="multipart/form-data">
+    Upload a File:
+    <input type="file" id="myfile" name="myfile">
+    <input type="submit" id="submit" name="submit" value="Upload File Now" >
+</form>
+<p id="status"></p>
+<script type="text/javascript" src="fileUpload.js"></script>
+</body>
 </html>
