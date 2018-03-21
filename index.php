4334a9be4eb54df54bf39dd17f0d6a4d4f020615
Unknown <maxime.dedobbeleer@gmail.com>
2018-03-21 18:59:52 +0000

    Nav update on index.php

--- index.php
+++ index.php
@@ -70,7 +70,10 @@ z-index: 10000;
             <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">My events</span></a>
           </div>
           <div class="sectionfix">
-            <a href="#" class="item"><span class="icon"><i class="fa fa-plus"></i></span></a>
+            <a href="#" class="item"><span class="icon"><i class="fa fa-plus"></i></span><span class="name">Make an event</span></a>
+          </div>
+          <div class="sectionfix">
+            <a href="#" class="item"><span class="icon"><i class="fas fa-building"></i></span><span class="name">Book a room</span></a>
           </div>
           <div class="sectionfix">
             <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">Event management</span></a>
@@ -112,6 +115,7 @@ z-index: 10000;
           <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">My meetings</span></a>
           <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">My events</span></a>
           <a href="bookaroom.php" class="item"><span class="icon"><i class="fa fa-calendar-plus"></i></span><span class="name">Make an event</span></a>
+          <a href="bookaroom.php" class="item"><span class="icon"><i class="fas fa-building"></i></span><span class="name">Book a room</span></a>
           <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-alt"></i></span><span class="name">Event management</span></a>
         </div>
         <div class="main">
