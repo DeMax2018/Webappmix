4ca8c9940aa8ee5cfb19f3a60e7305b794f431dd
nicklangens <nl210999@stud.miniemeninstituut.be>
2018-03-22 13:57:31 +0100

    hier

--- middleman.php
+++ middleman.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include"conn.php";
-$_GET["request"] = "event";
+$_GET["request"] = "rent";
 $_SESSION["userid"] = 1;
 $grandaccescreateevent = "false";
 $getcredentials = $dbh->prepare("SELECT * FROM privaterights WHERE UserID = ".$_SESSION["userid"]);
@@ -10,8 +10,6 @@ $credentials = $getcredentials->fetch(PDO::FETCH_ASSOC);
 if($credentials["Create_events"] == 1){
   $grandaccescreateevent = "true";
 }
-
-echo $grandaccescreateevent;
 if(isset($_GET["request"]) and $_GET["request"] === "event"){
   if($grandaccescreateevent === "true"){
     $_SESSION["bookaroom"] = "event";
