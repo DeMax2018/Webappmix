d37a9cdcc9158abe689c2cd8c102b18f684e8a2a
Unknown <maxime.dedobbeleer@gmail.com>
2018-03-22 13:36:11 +0000

    Nav is good now

--- index.php
+++ index.php
@@ -70,32 +70,23 @@ z-index: 10000;
             <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">My events</span></a>
           </div>
           <div class="sectionfix">
-            <a href="#" class="item"><span class="icon"><i class="fa fa-plus"></i></span><span class="name">Make an event</span></a>
+            <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-plus"></i></span><span class="name">Make an event</span></a>
           </div>
           <div class="sectionfix">
             <a href="#" class="item"><span class="icon"><i class="fas fa-building"></i></span><span class="name">Book a room</span></a>
           </div>
           <div class="sectionfix">
-            <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-check"></i></span><span class="name">Event management</span></a>
+            <a href="#" class="item"><span class="icon"><i class="fa fa-calendar-alt"></i></span><span class="name">Event management</span></a>
           </div>
         </div>
         <div class="fixit">
-          <div class="sectionfix">
-            <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Newsfeed</span></a>
-          </div>
           <div class="sectionfix">
             <a href="#" class="item active"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Account management</span></a>
           </div>
           <div class="sectionfix">
-            <a href="#" class="item"><span class="icon"><i class="fa fa-users"></i></span><span class="name">Meeting management</span></a>
+            <a href="#" class="item"><span class="icon"><i class="fas fa-building"></i></span><span class="name">Room & building management</span></a>
           </div>
         </div>
-        <div class="fixit">
-          <div class="sectionfix">
-            <a href="#" class="item"><span class="icon"><i class="fa fa-exclamation"></i></span><span class="name">Admin newsfeed</span></a>
-          </div>
-        </div>
-
       </div>
     </div>
   </nav>
